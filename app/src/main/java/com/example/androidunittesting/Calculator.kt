2b8b6f55302292f1a77f959e94184abb566c81e4
nalini <nalinikanta7978@gmail.com>
2022-03-02 17:01:37 +0530

    j unit test

---
+++ app/src/main/java/com/example/androidunittesting/Calculator.kt
@@ -0,0 +1,10 @@
+package com.example.androidunittesting
+
+class Calculator() {
+    companion object{
+        fun addition(num1:Int,num2:Int):Int{
+            return 8
+        }
+   }
+
+}
---
+++ app/src/test/java/com/example/androidunittesting/ExampleUnitTest.kt
@@ -0,0 +1,21 @@
+package com.example.androidunittesting
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+   @Test
+    fun is_addition_correct(){
+        assertEquals(9,Calculator.addition(5,4))
+    }
+}
